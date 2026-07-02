1c7e1763c34cbf782ccac4fc1e29aa93487f49f4
noahamnache-ship-it <noahamnache@gmail.com>
2026-07-02 10:40:35 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,4 +1,9 @@
 const calcul = require('./app');
+
 test('adds 1 + 2 to equal 3', () => {
   expect(calcul(1, 2)).toBe(3);
+});
+
+test('adds 11 + 2 to equal 13', () => {
+  expect(calcul(11, 2)).toBe(13);
 });
